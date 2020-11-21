fc39011958c6693597df58f6741b65156746feb7
Pavel Ivanov <pafelka@gmail.com>
2020-11-22 01:40:17 +0200

    stage

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Link from "next/link";
 import axios from "axios";
+import gsap from 'gsap';
+import { validateEmailFormat } from "@utils/helpers";
 import { Container, Row, Col, Input, Label } from "reactstrap";
 
 const navItems = [
@@ -8,23 +10,142 @@ const navItems = [
   { id: 3, idnm: "pricing", navheading: "Pricing" },
 ];
 
+
+const runAnimation = (button) => {
+  const getVar = variable => getComputedStyle(button).getPropertyValue(variable);
+  if (!button.classList.contains('active')) {
+
+    button.classList.add('active');
+
+    gsap.to(button, {
+      keyframes: [{
+        '--left-wing-first-x': 50,
+        '--left-wing-first-y': 100,
+        '--right-wing-second-x': 50,
+        '--right-wing-second-y': 100,
+        duration: .2,
+        onComplete() {
+          gsap.set(button, {
+            '--left-wing-first-y': 0,
+            '--left-wing-second-x': 40,
+            '--left-wing-second-y': 100,
+            '--left-wing-third-x': 0,
+            '--left-wing-third-y': 100,
+            '--left-body-third-x': 40,
+            '--right-wing-first-x': 50,
+            '--right-wing-first-y': 0,
+            '--right-wing-second-x': 60,
+            '--right-wing-second-y': 100,
+            '--right-wing-third-x': 100,
+            '--right-wing-third-y': 100,
+            '--right-body-third-x': 60
+          })
+        }
+      }, {
+        '--left-wing-third-x': 20,
+        '--left-wing-third-y': 90,
+        '--left-wing-second-y': 90,
+        '--left-body-third-y': 90,
+        '--right-wing-third-x': 80,
+        '--right-wing-third-y': 90,
+        '--right-body-third-y': 90,
+        '--right-wing-second-y': 90,
+        duration: .2
+      }, {
+        '--rotate': 50,
+        '--left-wing-third-y': 95,
+        '--left-wing-third-x': 27,
+        '--right-body-third-x': 45,
+        '--right-wing-second-x': 45,
+        '--right-wing-third-x': 60,
+        '--right-wing-third-y': 83,
+        duration: .25
+      }, {
+        '--rotate': 55,
+        '--plane-x': -8,
+        '--plane-y': 24,
+        duration: .2
+      }, {
+        '--rotate': 60,
+        '--plane-x': 45,
+        '--plane-y': -380,
+        '--plane-opacity': 0,
+        duration: .3,
+        onComplete() {
+          setTimeout(() => {
+            button.removeAttribute('style');
+            gsap.fromTo(button, {
+              opacity: 0,
+              y: -8
+            }, {
+              opacity: 1,
+              y: 0,
+              clearProps: true,
+              duration: .3,
+              onComplete() {
+                button.classList.remove('active');
+                // cb();
+              }
+            })
+          }, 1000)
+        }
+      }]
+    })
+
+    gsap.to(button, {
+      keyframes: [{
+        '--text-opacity': 0,
+        '--border-radius': 0,
+        '--left-wing-background': getVar('--primary-darkest'),
+        '--right-wing-background': getVar('--primary-darkest'),
+        duration: .1
+      }, {
+        '--left-wing-background': getVar('--primary'),
+        '--right-wing-background': getVar('--primary'),
+        duration: .1
+      }, {
+        '--left-body-background': getVar('--primary-dark'),
+        '--right-body-background': getVar('--primary-darkest'),
+        duration: .4
+      }, {
+        '--success-opacity': 1,
+        '--success-scale': 1,
+        duration: .25,
+        delay: .25
+      }]
+    })
+
+  }
+}
+
+
 const Footer = () => {
+  const refButton = useRef();
   const refEmail = useRef();
+
+  const [state, setState] = useState({
+    btnIsDisabled: true,
+  });
 
   const subscribeHandler = async (e) => {
     e.preventDefault();
-    const { current } = refEmail;
-    const { props } = current;
-    const email = current.value
+    const { current: elEmail } = refEmail;
+    const email = elEmail.value;
+    elEmail.value = "";
 
-    try {
-      const r = await axios.post("https://api.teachablehub.com/v1/earlyaccess", {
-        email,
-        only_newsletter: true
-      });
-    } catch (e) {
-      console.error(e);
-    }
+    const { current: button } = refButton;
+    runAnimation(button);
+
+    console.log('@@@', email);
+
+    // try {
+    //   const r = await axios.post("https://api.teachablehub.com/v1/earlyaccess", {
+    //     email,
+    //     only_newsletter: true
+    //   });
+    // } catch (e) {
+    //   console.error(e);
+    // }
   }
 
   return (
@@ -73,14 +194,34 @@ const Footer = () => {
                 <Col lg="12">
                   <div className="foot-subscribe form-group position-relative">
                     <Label>Write your email <span className="text-danger">*</span></Label>
-                    <Input innerRef={refEmail} type="email" name="email" id="emailsubscribe" className="form-control rounded" placeholder="Your email ... " required />
+                    <Input
+                      innerRef={refEmail}
+                      type="email"
+                      name="email"
+                      id="emailsubscribe"
+                      className="form-control rounded"
+                      placeholder="Your email ... "
+                      required
+                      onChange={({ target }) => {
+                        const email = target.value;
+                        setState({
+                          btnIsDisabled: (email === "" || !validateEmailFormat(email))
+                        });
+                      }}
+                    />
                     <p className="th-text-small text-muted pt-1" style={{ color: "#fff", opacity: .6 }}>We respect your privacy and we’ll never share your details.</p>
                   </div>
                 </Col>
                 <Col lg="12">
-                  <a className="btn btn-primary btn-block shadow-none" onClick={subscribeHandler}>
+                  {/* <a className="btn btn-primary btn-block shadow-none" onClick={subscribeHandler}>
                     Subscribe
-                  </a>
+                  </a> */}
+                  <button ref={refButton} className="emailButton" disabled={state.btnIsDisabled} onClick={subscribeHandler}>
+                    <span className="default">Subscribe</span>
+                    {/* <span className="success text-muted">Sent</span> */}
+                    <div className="left"></div>
+                    <div className="right"></div>
+                  </button>
                 </Col>
               </Row>
             </Col>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -39,3 +39,10 @@ export const ScrollToHash = () => {
 
   return null;
 }
+
+
+export const validateEmailFormat = (email) => {
+  // Copied from existing website's newsletter_signup.js
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(email);
+}
